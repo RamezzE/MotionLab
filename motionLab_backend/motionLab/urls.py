2db1b479b943ac8916f3d41f3533a859e7575ad5
Abdelrahman Emad <abdelrahmansat2003@gmail.com>
2024-11-18 23:48:18 +0200

    set up auth

--- motionLab_backend/motionLab/urls.py
+++ motionLab_backend/motionLab/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SignupView, LoginView, VideoView
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(), name='login'),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
     path('videos/', VideoView.as_view(), name='videos'),
 ]
--- motionLab_backend/motionLab/views.py
+++ motionLab_backend/motionLab/views.py
@@ -1,3 +1,4 @@
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.shortcuts import render
 
 from rest_framework.views import APIView
@@ -10,26 +11,34 @@ from .serializers import UserSerializer, VideoSerializer
 
 
 class SignupView(APIView):
-    permission_classes = [AllowAny]
-
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(APIView):
-    permission_classes = [AllowAny]
-
     def post(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
         user = authenticate(request, email=email, password=password)
-        if user:
-            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
-        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    "message": "Login successful",
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"error": "Invalid email or password"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
 
 
 class VideoView(APIView):
