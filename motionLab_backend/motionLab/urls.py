b7311a514b03459d2c558984b244571aa64ce9b7
Ramez Ehab <ramezehab2@gmail.com>
2025-02-18 10:12:56 +0200

    chore: delete extra backend folder

--- motionLab_backend/motionLab/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignupView, LoginView, VideoView
-
-urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="login"),
-    path('videos/', VideoView.as_view(), name='videos'),
-]
